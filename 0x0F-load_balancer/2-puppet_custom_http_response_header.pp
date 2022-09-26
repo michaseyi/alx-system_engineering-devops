44dd488f59f85f1d7f420c5bdfb49146dfc1490a
Adewole Michael <michaseyi@gmail.com>
2022-09-27 00:55:36 +0100

    Added task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,11 @@
 # configures nginx on a server
-$config = 'server {
-  listen 80 default_server;
-  listen [::]:80 default_server;
-  
-  root /var/www/html;
-  index index.html index.htm index.nginx-debian.html;
-  add_header X-Served-By $HOSTNAME;
-}
-'
 package { 'nginx':
   ensure => 'installed',
 }
 
-file { 'server_config':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  content => $config
+exec { 'server_config':
+  command => "sed -i '/server {/a add_header X-Served-By \$hostname;' /etc/nginx/sites-available/default",
+  path    => ['/usr/sbin', '/usr/bin']
 }
 
 exec { 'service nginx restart':
