8f9f317a0b5577b784f0cf20285c6d3dba059746
Adewole Michael <michaseyi@gmail.com>
2022-09-27 01:41:35 +0100

    Added task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,8 @@
 # configures nginx on a server
-
-exec { 'install_nginx':
-  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx;',
-  provider => shell
+exec { 'setup_nginx':
+  command  => 'apt-get -y update; apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $hostname;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
 
-exec { 'nginx_config':
-  command  => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default;',
-  provider => shell
-}
-
-exec { 'restart_nginx':
-  command  => 'sudo service nginx restart',
-  provider => shell
-}
-
-#exec { 'command':
-#  command  => 'apt-get -y update;
-#  apt-get -y install nginx;
-#  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-#  service nginx restart',
-#  provider => shell,
-#}
