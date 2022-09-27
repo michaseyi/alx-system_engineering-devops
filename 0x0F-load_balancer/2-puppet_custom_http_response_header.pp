2f9e84f0f17432a95cca73190e72cb1ccf6ee6f6
Adewole Michael <michaseyi@gmail.com>
2022-09-27 01:24:04 +0100

    Added task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,10 +4,8 @@ package { 'nginx':
 }
 
 exec { 'server_config':
-  command  => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  command  => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
   provider => shell,
 }
 
-exec { 'service nginx restart':
-  provider => shell,
-}
